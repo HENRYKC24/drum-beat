8c1aca5424c98123314daa6af707f5ec31bc4dae
HENRY-KC <henrykc24@yahoo.com>
2020-05-06 12:01:12 +0100

    feature: the keyboard is used to play the band

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
+function runSwitch( letter ) {
+  switch ( letter ) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'k':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case 'l':
+      var kick = new Audio('sounds/kick.mp3');
+      kick.play();
+      break;
+    default:
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+  };
+}
+
+function changeTextColor( letter ) {
+  for( var i = 0; i < allButtons.length; i++ ) {
+    if ( allButtons[i].textContent !== letter ) {
+      allButtons[i].style.color = '#DA0463';
+    } else {
+      allButtons[i].style.color = '#fff';
+    }
+  };
+}
 var allButtons = document.querySelectorAll('button');
+var pressedKey;
+document.addEventListener('keypress', function(event) {
+  var letter = event.key.toLowerCase();
+  changeTextColor(letter);
+  runSwitch(letter);
+})
 for( var i = 0; i < allButtons.length; i++ ) {
   allButtons[i].addEventListener('click', function(e) {
     var thisText = this.textContent;
     //change all text back to red except the clicked which will be white
-    for( var i = 0; i < allButtons.length; i++ ) {
-      if ( allButtons[i].textContent !== thisText ) {
-        allButtons[i].style.color = '#DA0463';
-      } else {
-        allButtons[i].style.color = '#fff';
-      }
-    };
+    changeTextColor(thisText);
     //play sound based on the button clicked
-    switch ( thisText ) {
-      case 'w':
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-        break;
-      case 'a':
-        var tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-        break;
-      case 's':
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-      case 'd':
-        var tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-      case 'j':
-        var snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-      case 'k':
-        var crash = new Audio('sounds/crash.mp3');
-        crash.play();
-        break;
-      case 'l':
-        var kick = new Audio('sounds/kick.mp3');
-        kick.play();
-        break;
-      default:
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-    };
+    runSwitch(thisText);
   });
 };

573be0e96378c3f0072abd6e48de5cfb8674797b
HENRY-KC <henrykc24@yahoo.com>
2020-05-06 20:39:14 +0100

    fix: key f works

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function runSwitch( letter ) {
   switch ( letter ) {
-    case 'w':
+    case 'a':
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
       break;
-    case 'a':
+    case 's':
       var tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
       break;
-    case 's':
+    case 'd':
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
-    case 'd':
+    case 'f':
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
